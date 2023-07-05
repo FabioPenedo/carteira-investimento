2f0f4cd31080a70e58fc56b246f8a30bec010603
Penedo <fabiopenedo21@gmail.com>
2023-07-04 22:54:23 -0300

    pequeno ajuste no no .toFixed para o número em porcentagem de renda fixa e renda variavel

--- script.js
+++ script.js
@@ -85,25 +85,16 @@ function inserirDados(dados) {
 
 
   if(porcentagensPalavras.rf == 100) {
-    qS('#divisao-modalidade #rf').innerHTML = `${porcentagensPalavras.rf.toFixed(2)} % Renda Fixa`
+    qS('#divisao-modalidade #rf').innerHTML = `${porcentagensPalavras.rf}% Renda Fixa`
     qS('#divisao-modalidade #rv').innerHTML = `0% Renda Variável`
   } else if(porcentagensPalavras.rv == 100) {
     qS('#divisao-modalidade #rf').innerHTML = `0% Renda Fixa`
-    qS('#divisao-modalidade #rv').innerHTML = `${porcentagensPalavras.rv.toFixed(2)}% Renda Variável`
+    qS('#divisao-modalidade #rv').innerHTML = `${porcentagensPalavras.rv}% Renda Variável`
   } else if(porcentagensPalavras.rf || porcentagensPalavras.rv) {
     qS('#divisao-modalidade #rf').innerHTML = `${porcentagensPalavras.rf.toFixed(2)}% Renda Fixa`
     qS('#divisao-modalidade #rv').innerHTML = `${porcentagensPalavras.rv.toFixed(2)}% Renda Variável`
   }
 
-  /*console.log(porcentagensPalavras.rf)
-  for(let i in porcentagensPalavras) {
-    //console.log(porcentagensPalavras[i])
-    //
-  }*/
-
-  //qS('#cateira #divisao-modalidade').innerHTML = `${porcentagensPalavras[rf]}%`
-
-  //console.log(porcentagensPalavras); // Exibir as porcentagens das palavras no console
 }
 
 
